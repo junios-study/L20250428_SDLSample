7abb345f1a8c15a31656272b78bb2126e4422192
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-28 17:13:44 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "SDL3/SDL.h"
+
+#pragma comment(lib, "SDL3")
+
+int main()
+{
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+
+	SDL_Window* MyWindow = SDL_CreateWindow("Sample", 640, 480, SDL_WINDOW_OPENGL);
+
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(MyWindow);
+
+	SDL_Quit();
+
+	return 0;
+}
